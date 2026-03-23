5d1a9e38b0e24811ecc2feb0b6cc4daf35b2deaa
Robin Fechner <Robin.fech@googlemail.com>
2026-03-23 10:24:36 +0100

    Initial site

---
+++ src/data/projects.ts
@@ -0,0 +1,29 @@
+export type Project = {
+  id: string;
+  title: string;
+  description: string;
+  tags: string[];
+  href: string;
+  linkLabel: string;
+};
+
+export const projects: Project[] = [
+  {
+    id: "varneth-code",
+    title: "Varneth Code",
+    description:
+      "Textadventure QuellCode",
+    tags: ["Java", "OOP", "Backend"],
+    href: "https://github.com/Sloth-blip/textadventure-varneth",
+    linkLabel: "GitHub öffnen",
+  },
+  {
+    id: "varneth-demo",
+    title: "Varneth Spiel-Demo als Webapp",
+    description:
+      "",
+    tags: ["Demo", "Vue", "Frontend"],
+    href: "https://varneth.quest",
+    linkLabel: "Demo spielen",
+  },
+];
